91d8ed639c8c092d3fb1677e3096e78b83d7ea1a
Jaynam <jaynam999@gmail.com>
2017-04-14 20:12:28 +0530

    file storage and ui changes

--- js/firebase-helper.js
+++ js/firebase-helper.js
@@ -2,8 +2,14 @@ this.submitLoginButton = document.getElementById('#submit');
 this.storageRef = firebase.storage().ref();
 this.databaseRef = firebase.database().ref();
 
-function deleteFile(key, uid, fileName) {
-    var deleteRef = storageRef.child("client/" + uid + "/" + fileName);
+var adminFlag = "false";
+var adminSelectUid;
+
+function deleteFile(key, uid) {
+    console.log("Deleting");
+    firebase.database().ref("files/" + uid + "/").child(key).remove();
+    location.reload();
+    /*var deleteRef = storageRef.child("client/" + uid + "/" + fileName);
 
     // Delete the file
     deleteRef.delete().then(function() {
@@ -12,6 +18,35 @@ function deleteFile(key, uid, fileName) {
         location.reload();
     }).catch(function(error) {
         console.log(error);
+    });*/
+
+}
+
+$('#userSelect').on('change', function() {
+    if (this.value == "all") {
+        $(".display-section").empty();
+        location.reload();
+    } else {
+        $(".display-section").empty();
+        getFiles(this.value);
+    }
+
+});
+
+$('#adminSelect').on('change', function() {
+    adminSelectUid = this.value;
+});
+
+function getFiles(uid) {
+    databaseRef = firebase.database().ref("files/" + uid);
+    databaseRef.once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
+            if (childKey) {
+                $('#filetable').append('<li class="list-group-item">' + childData.fileName + '<br><a href="' + childData.downloadUrl + '" target="_blank">View</a> <a href="' + childData.downloadUrl + '" download>download</a> <a href="#" onClick="deleteFile(\'' + childKey + '\',\'' + uid + '\')">delete</a></li>');
+            }
+        });
     });
 
 }
@@ -19,8 +54,9 @@ function deleteFile(key, uid, fileName) {
 
 
 firebase.auth().onAuthStateChanged(function(user) {
+
     if (user) {
-        console.log("Logged");
+        $('#myModal').modal('hide');
         $("#login").css("display", "none");
         $("#signOut").css("display", "inline");
         var user = getCurrentuser();
@@ -30,19 +66,29 @@ firebase.auth().onAuthStateChanged(function(user) {
             email = user.email;
             uid = user.uid;
         }
-        databaseRef = firebase.database().ref("files/" + uid);
-        databaseRef.once('value', function(snapshot) {
+        adminRef = firebase.database().ref("admins/");
+        adminRef.once('value', function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
-                var childKey = childSnapshot.key;
-                var childData = childSnapshot.val();
-                console.log(childData);
-                if (childKey) {
-                    $(".right-section").css("display", "block");
-                    $('#filetable').append('<li class="list-group-item">' + childData.fileName + '<br><a href="' + childData.downloadUrl + '" target="_blank">View</a> <a href="' + childData.downloadUrl + '" download>download</a> <a href="#" onClick="deleteFile(\'' + childKey + '\',\'' + uid + '\',\'' + childData.fileName + '\')">delete</a></li>');
+                var adminKey = childSnapshot.key;
+                if (adminKey == uid) {
+                    adminFlag = true;
+                    userRef = firebase.database().ref("users/");
+                    userRef.once('value', function(snapshot) {
+                        snapshot.forEach(function(childSnapshot) {
+                            var userKey = childSnapshot.key;
+                            var userData = childSnapshot.val();
+                            $('#userSelect').append('<option value=' + userData.uid + '>' + userData.emailId + '</option>');
+                            $('#adminSelect').append('<option value=' + userData.uid + '>' + userData.emailId + '</option>');
+                            getFiles(userKey);
 
+                        });
+                    });
+                } else {
+                    getFiles(uid);
                 }
 
             });
+
         });
 
         console.log(email + " User Logged In");
@@ -93,3 +139,95 @@ $('#submitSignIn').click(function() {
             alert(error.message);
         });
 });
+
+function FriendlyChat() {
+    this.checkSetup();
+
+    this.submitImageButton = document.getElementById('submitImage');
+    this.mediaCapture = document.getElementById('mediaCapture');
+    this.storageRef = firebase.storage().ref();
+    this.databaseRef = firebase.database().ref();
+
+
+    // Events for image upload.
+    this.submitImageButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        this.mediaCapture.click();
+    }.bind(this));
+    this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
+
+
+}
+
+
+
+// Saves a new message containing an image URI in Firebase.
+// This first saves the image in Firebase storage.
+FriendlyChat.prototype.saveImageMessage = function(event) {
+    event.preventDefault();
+    var file = event.target.files[0];
+
+    // Clear the selection in the file picker input.
+
+
+    // Check if the file is an image.
+    if (!file.type.match('application/pdf')) {
+        var data = {
+            message: 'You can only add pdf',
+            timeout: 2000
+        };
+        alert(data.message);
+        return;
+    }
+
+
+    // Upload the image to Cloud Storage.
+    var dataRef = this.databaseRef;
+    var filePath = file.name;
+    var user = firebase.auth().currentUser;
+
+
+    if (adminFlag) {
+        storeFile(adminSelectUid);
+    } else {
+        storeFile(user.uid);
+    }
+
+    function storeFile(uid) {
+        this.storageRef.child("client/" + user.uid + "/" + filePath).put(file)
+            .then(function(snapshot) {
+                console.log('Uploaded a blob or file!', snapshot);
+                dataRef.child("files").child(uid).push({
+                    fileName: snapshot.metadata.name,
+                    path: snapshot.metadata.fullPath,
+                    size: snapshot.metadata.size,
+                    downloadUrl: snapshot.downloadURL
+                })
+                location.reload();
+            }).catch(function(error) {
+                console.log(error.code);
+                alert(error.message);
+            });
+
+    }
+
+}
+
+// Checks that the Firebase SDK has been correctly setup and configured.
+FriendlyChat.prototype.checkSetup = function() {
+    if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+        window.alert('You have not configured and imported the Firebase SDK. ' +
+            'Make sure you go through the codelab setup instructions.');
+    } else if (config.storageBucket === '') {
+        window.alert('Your Cloud Storage bucket has not been enabled. Sorry about that. This is ' +
+            'actually a Firebase bug that occurs rarely. ' +
+            'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+            'and make sure the storageBucket attribute is not empty. ' +
+            'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
+            'displayed there.');
+    }
+};
+
+window.onload = function() {
+    window.friendlyChat = new FriendlyChat();
+};
