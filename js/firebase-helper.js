ea508ea997d1a814f137a983428a036bd17233c9
Jaynam <jaynam999@gmail.com>
2017-06-12 14:55:22 +0530

    add/delete

--- js/firebase-helper.js
+++ js/firebase-helper.js
@@ -2,14 +2,17 @@ this.submitLoginButton = document.getElementById('#submit');
 this.storageRef = firebase.storage().ref();
 this.databaseRef = firebase.database().ref();
 
-var adminFlag = "false";
+var adminFlag = false;
 var adminSelectUid;
 
+
+//Session Logout
 var idleTime = 0;
 $(document).ready(function() {
 
+
+
     if (getCurrentuser()) {
-        console.log(getCurrentuser);
         $("#login").css("display", "none");
         $("#myfiles").css("display", "inline");
         $("#signOut").css("display", "inline");
@@ -41,10 +44,9 @@ function timerIncrement() {
 
 }
 
-
-function deleteFile(key, uid) {
-    console.log("Deleting");
-    firebase.database().ref("files/" + uid + "/").child(key).remove();
+//Delete File 
+function deleteFile(key, folderkey, uid) {
+    firebase.database().ref("files/" + uid + "/" + folderkey + "/").child(key).remove();
     location.reload();
     /*var deleteRef = storageRef.child("client/" + uid + "/" + fileName);
 
@@ -64,7 +66,7 @@ $('#userSelect').on('change', function() {
         $(".display-section").empty();
         location.reload();
     } else {
-        $(".display-section").empty();
+        $(".filetable").empty();
         getFiles(this.value);
     }
 
@@ -72,24 +74,40 @@ $('#userSelect').on('change', function() {
 
 $('#adminSelect').on('change', function() {
     adminSelectUid = this.value;
+    $('#folderSelect').empty();
+    getFolders(adminSelectUid);
 });
 
+//Get Files Of Client
 function getFiles(uid) {
     databaseRef = firebase.database().ref("files/" + uid);
     databaseRef.once('value', function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
-            var childKey = childSnapshot.key;
-            var childData = childSnapshot.val();
-            if (childKey) {
-                $('#filetable').append('<li class="list-group-item"> <p>' + childData.fileName + '</p><a href="' + childData.downloadUrl + '" target="_blank">View <i class="fa fa-eye"></i></a> <a href="' + childData.downloadUrl + '" download>Download <i class="fa fa-download "></i></a> <a href="#" onClick="deleteFile(\'' + childKey + '\',\'' + uid + '\')">delete <i class="fa fa-trash"></i></a></li>');
+            var folderKey = childSnapshot.key;
+            var folderData = childSnapshot.val();
+
+            if (folderKey) {
+                $('.filetable').append('<div class="panel panel-default"><div class="panel-heading" role="tab" id="headingOne"><h4 class="panel-title"><a role="button" data-toggle="collapse" data-parent="#accordion" href="#' + folderKey + '" aria-expanded="false" aria-controls="' + folderKey + '">' + folderKey + '</a></h4></div><div id="' + folderKey + '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne"><ul class="list-group ' + folderKey + '"></ul></div>');
+
+                databaseFolderRef = firebase.database().ref("files/" + uid + "/" + folderKey);
+                databaseFolderRef.once('value', function(snapshot) {
+                    snapshot.forEach(function(childSnapshot) {
+                        var childKey = childSnapshot.key;
+                        var childData = childSnapshot.val();
+
+                        $('.' + folderKey).append('<li class="list-group-item"> <p>' + childData.fileName + '</p><a href="' + childData.downloadUrl + '" target="_blank">View <i class="fa fa-eye"></i></a> <a href="' + childData.downloadUrl + '" download>Download <i class="fa fa-download "></i></a> <a href="#" onClick="deleteFile(\'' + childKey + '\',\'' + folderKey + '\' ,\'' + uid + '\')">delete <i class="fa fa-trash"></i></a></li>');
+
+                    });
+                    $('.filetable').append('</ul></div>');
+                });
             }
+
         });
     });
 
 }
 
-
-
+// To check the login state
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         $('#myModal').modal('hide');
@@ -107,25 +125,32 @@ firebase.auth().onAuthStateChanged(function(user) {
         adminRef.once('value', function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
                 var adminKey = childSnapshot.key;
-                if (adminKey == uid) {
-                    adminFlag = true;
-                    adminSelectUid = uid;
-                    $("#texthint").show();
-                    $(".selectbtn").css("display", "block");
-                    userRef = firebase.database().ref("users/");
-                    userRef.once('value', function(snapshot) {
-                        snapshot.forEach(function(childSnapshot) {
-                            var userKey = childSnapshot.key;
-                            var userData = childSnapshot.val();
-                            $('#userSelect').append('<option value=' + userData.uid + '>' + userData.emailId + '</option>');
-                            $('#adminSelect').append('<option value=' + userData.uid + '>' + userData.emailId + '</option>');
-                            getFiles(userKey);
 
+                if (adminFlag == false) {
+                    if (adminKey == uid) {
+                        adminFlag = true;
+                        console.log(uid);
+                        adminSelectUid = uid;
+                        $("#texthint").show();
+                        $(".selectbtn").css("display", "block");
+                        $('#adminSelect').append('<option value="">Select User</option>');
+
+                        userRef = firebase.database().ref("users/");
+                        userRef.once('value', function(snapshot) {
+                            snapshot.forEach(function(childSnapshot) {
+                                var userKey = childSnapshot.key;
+                                var userData = childSnapshot.val();
+                                $('#userSelect').append('<option value=' + userData.uid + '>' + userData.emailId + '</option>');
+                                $('#adminSelect').append('<option value=' + userData.uid + '>' + userData.emailId + '</option>');
+                                //$('#folderSelect').prop("disabled", "disabled");
+
+                            });
                         });
-                    });
-                } else {
-                    console.log("Non admin")
-                    getFiles(uid);
+                        return;
+                    } else {
+                        getFiles(uid);
+                        getFolders(uid);
+                    }
                 }
 
             });
@@ -133,7 +158,6 @@ firebase.auth().onAuthStateChanged(function(user) {
         });
 
     } else {
-        console.log("No Users");
         $("#myfiles").css("display", "none");
         $("#signOut").css("display", "none");
         $("#login").css("display", "inline");
@@ -184,6 +208,30 @@ $('#submitSignIn').click(function() {
             alert(error.message);
         });
 });
+
+
+
+function getFolders(uid) {
+    databaseRef = firebase.database().ref("files/" + uid);
+    databaseRef.once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+
+            var folderKey = childSnapshot.key;
+            var folderData = childSnapshot.val();
+
+            $('#folderSelect').append('<option value=' + folderKey + '>' + folderKey + '</option>');
+        });
+    });
+    $('#folderSelect').append('<option value="newfolder">Add New Folder</option>');
+}
+$('#folderSelect').on('change', function() {
+    if (this.value == "newfolder") {
+        $("#txtFolderName").css({ "display": "block" });
+    } else {
+        $("#txtFolderName").css({ "display": "none" });
+    }
+});
+
 
 function FriendlyChat() {
     this.checkSetup();
@@ -236,20 +284,23 @@ FriendlyChat.prototype.saveImageMessage = function(event) {
     var dataRef = this.databaseRef;
     var filePath = file.name;
     var user = firebase.auth().currentUser;
-
-
-    if (adminFlag == true) {
-        storeFile(adminSelectUid);
-    } else {
-        storeFile(user.uid);
+    var folderName = $("#folderSelect").val();
+    if (folderName == "newfolder") {
+        folderName = $("#txtFolderName").val();
     }
 
-    function storeFile(uid) {
+    if (adminFlag == true) {
+        storeFile(adminSelectUid, folderName);
+    } else {
+        storeFile(user.uid, folderName);
+    }
+
+    function storeFile(uid, folderName) {
         $("#progress-part").css('display', 'block');
         this.storageRef.child("client/" + user.uid + "/" + filePath).put(file)
             .then(function(snapshot) {
                 alert("Uploaded a file!");
-                dataRef.child("files").child(uid).push({
+                dataRef.child("files").child(uid).child(folderName).push({
                     fileName: snapshot.metadata.name,
                     path: snapshot.metadata.fullPath,
                     size: snapshot.metadata.size,
@@ -258,13 +309,18 @@ FriendlyChat.prototype.saveImageMessage = function(event) {
 
                 location.reload();
             }).catch(function(error) {
-                console.log(error.code);
-                alert.log(error.message);
+                console.log(error);
+                $("#progress-part").css('display', 'none');
+                alert("PLease select folde and file properly!");
             });
 
     }
 
 }
+
+
+
+
 
 // Checks that the Firebase SDK has been correctly setup and configured.
 FriendlyChat.prototype.checkSetup = function() {
